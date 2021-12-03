343eb4fd366fadb6dc849868140168be6d89f685
Rishit Pandey <rishpandey8097@gmail.com>
2021-12-03 16:16:32 +0530

    add tests for app.tsx

---
+++ src/App.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react'
+import App from './App'
+
+test('app should renders', () => {
+  const { getByText } = render(<App />)
+  const linkElement = getByText(
+    /An Electron boilerplate including TypeScript, React, Jest and ESLint./i
+  )
+  expect(linkElement).toBeInTheDocument()
+})
