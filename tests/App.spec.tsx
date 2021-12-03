1c4444d367df01c81f2c80085fd3748dfef24d1e
Rishit Pandey <rishpandey8097@gmail.com>
2021-12-03 16:59:10 +0530

    DOCS: app.spec.tsx

--- tests/App.spec.tsx
+++ tests/App.spec.tsx
@@ -1,10 +1,14 @@
 import { render } from '@testing-library/react'
 import App from '../src/App'
 
-test('app should renders', () => {
+test('app should render', () => {
   const { getByText } = render(<App />)
+
+  // use regular expression to check whether the given text exists in the rendered app.
   const linkElement = getByText(
     /An Electron boilerplate including TypeScript, React, Jest and ESLint./i
   )
+
+  // check if the link element exists in the document.
   expect(linkElement).toBeInTheDocument()
 })
